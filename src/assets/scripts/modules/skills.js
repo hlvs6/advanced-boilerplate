b7dd4b49e8d4aa34e4adf902abb00dbe950d89eb
hlvs6 <hlvs6@yandex.ru>
2019-01-07 22:07:46 +0300

    Добавил темплейты для компонентов

---
+++ src/assets/scripts/modules/skills.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+
+const skill = {
+
+    template: '#skill'
+}
+
+const skillsRow = {
+    props: {
+        skillsGroupObj: Object
+    },
+    components: {
+        skill
+    },
+    template: '#skills-row'
+} 
+
+new Vue({
+    el: '#skills-component',
+    components: {
+        skillsRow
+    },
+    data() {
+        return {
+            skills: {}
+        }
+    },
+    created() {
+        this.skills = require('../../../data/skills.json')
+    },
+    template: '#skills-list',
+})
