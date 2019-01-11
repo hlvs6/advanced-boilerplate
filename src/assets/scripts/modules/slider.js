c84394f5d36754a9af9a31795edf0e38b383ff2f
VASILIY-PC\grechin <grechin@corteos.info>
2019-01-11 17:07:12 +0300

    добавил компоненты для слайдера

--- src/assets/scripts/modules/slider.js
+++ src/assets/scripts/modules/slider.js
@@ -1,1 +1,15 @@
-console.log("!");
+import Vue from 'vue'
+
+const sliderSelectInfo = {
+    template: '#slider-b-info-select',
+}
+
+const slider = {
+    template: '#slider'
+}
+
+new Vue({
+    el: '#slider-root',
+    components: { sliderSelectInfo, slider },
+    template: '#slider-wrap',
+})
