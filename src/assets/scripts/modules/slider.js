ab7a21ad94a2a5f0e831f0ada79083d931511d7a
VASILIY-PC\grechin <grechin@corteos.info>
2019-01-14 12:24:51 +0300

    добавил работающий слайдер на страницу

--- src/assets/scripts/modules/slider.js
+++ src/assets/scripts/modules/slider.js
@@ -16,14 +16,49 @@ const selectSlide = {
 
 const sliderBtns = {
     template: '#sliderBtns',
+    props: {
+        works: Array,
+        index: Number
+    },
+    data() {
+        return {
+            prevButtonWorks: [],
+            nextButtonWorks: []
+        };
+    },
+    created() {
+        this.prevButtonWorks = this.transformWorksArrForBtn('prev');
+        this.nextButtonWorks = this.transformWorksArrForBtn('next');
+    },
     methods: {
         slide(direction) {
             this.$emit('slide', direction);
+        },
+        transformWorksArrForBtn(btnDirection) {
+
+            //Копирование массива при помощи spread оператора
+            const worksArray = [...this.works];
+
+            const lastItem = worksArray[worksArray.length - 1];
+
+            switch(btnDirection) {
+                case 'next':
+                    worksArray.push(worksArray[0]);
+                    worksArray.shift();
+                break;
+                case 'prev':
+                    worksArray.unshift(lastItem);
+                    worksArray.pop();
+                break;
+            }
+
+            return worksArray;
         }
     }
 }
 
 new Vue({
+
     el: '#slider-root',
     components: { sliderSelectInfo, selectSlide, sliderBtns },
     data() {
@@ -31,9 +66,6 @@ new Vue({
             works: [],
             currentIndex: 0
         }
-    },
-    created() {
-        this.works = require('../../../data/works.json');
     },
     computed: {
         currentWork() {
@@ -44,6 +76,9 @@ new Vue({
         currentIndex(value) {
             this.makeInfiniteSliding(value);
         }
+    },
+    created() {
+        this.works = require('../../../data/works.json');
     },
     methods: {
         makeInfiniteSliding(value) {
